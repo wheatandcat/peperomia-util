29028d364c5ac62308e609eee6226cbe61b6037d
wheatandcat <wheatandcat@gmail.com>
2020-02-24 18:38:48 +0900

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# peperomia-util
+
+ペペロミアで使用するする共通ライブラリ
+
+## build
+
+```
+$ yarn
+$ yarn tsc
+```
---
+++ src/theme.ts
@@ -0,0 +1,59 @@
+type ThemeColor = {
+  color: {
+    beige: string
+    blue: string
+    darkGray: string
+    dodgerBlue: string
+    gray: string
+    lightBlue: string
+    lightEmerald: string
+    lightGray: string
+    highLightGray: string
+    lightGreen: string
+    lightNavy: string
+    lightOrange: string
+    lightPink: string
+    lightRed: string
+    lightYellow: string
+    white: string
+    pink: string
+    main: string
+    red: string
+    sky: string
+    yellow: string
+    black: string
+  }
+}
+
+const baseColor = {
+  beige: '#E4E4C8',
+  blue: '#007bbb',
+  darkGray: '#4F4F4F',
+  dodgerBlue: '#A1CBF3',
+  gray: '#828282',
+  lightBlue: '#A7EFFF',
+  lightEmerald: '#98D181',
+  lightGray: '#D3D3D3',
+  highLightGray: '#F2F2F2',
+  lightGreen: '#ADCF01',
+  lightNavy: '#CECEE6',
+  lightOrange: '#FFE3AC',
+  lightPink: '#F1DBF9',
+  lightRed: '#FF8C8C',
+  lightYellow: '#FBFDB0',
+  white: '#fff',
+  pink: '#FCCBCB',
+  main: '#006835',
+  red: '#E15757',
+  sky: '#00C2ED',
+  yellow: '#f8e58c',
+  black: '#333631',
+}
+
+const getTheme = (): ThemeColor => {
+  return {
+    color: baseColor,
+  }
+}
+
+export default getTheme
